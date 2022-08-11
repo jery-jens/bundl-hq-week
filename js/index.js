fae4600a966f2933bd3d1615bac511886e35a33c
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-08-11 14:30:14 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("test!!")
+    const previous = document.querySelector(".top");
+    const next = document.querySelector(".bottom");
+
+    let dayIndex = 0;
+
+    previous.addEventListener("click", () => {
+        if (dayIndex !== 0) {
+            dayIndex = dayIndex - 1;
+            console.log(dayIndex);
+        };
+    });
+
+    next.addEventListener("click", () => {
+        if (dayIndex !== 4) {
+            dayIndex = dayIndex + 1;
+            console.log(dayIndex);
+        };
+    });
 });
