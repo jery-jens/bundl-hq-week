ad0e21b5b71375db9a849195f4b29607bf26c442
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-08-11 14:38:10 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,20 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
     const previous = document.querySelector(".top");
     const next = document.querySelector(".bottom");
+    const days = document.querySelectorAll(".day");
 
     let dayIndex = 0;
 
     previous.addEventListener("click", () => {
         if (dayIndex !== 0) {
             dayIndex = dayIndex - 1;
-            console.log(dayIndex);
+
+            for (let i = 0; i < days.length; i++) {
+                if (dayIndex === i) {
+                    days[i].classList.remove("hidden");
+                } else {
+                    days[i].classList.add("hidden");
+                };
+            };
         };
     });
 
     next.addEventListener("click", () => {
         if (dayIndex !== 4) {
             dayIndex = dayIndex + 1;
-            console.log(dayIndex);
+            
+            for (let i = 0; i < days.length; i++) {
+                if (dayIndex === i) {
+                    days[i].classList.remove("hidden");
+                } else {
+                    days[i].classList.add("hidden");
+                };
+            };
         };
     });
 });
